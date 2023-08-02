9b63a3fa187904852de1b05e904a9ce2ba97bb9c
kelpjr <kelv0046@e.ntu.edu.sg>
2023-08-02 22:39:00 +0800

    added buttons

---
+++ app/src/main/java/com/example/myapplicationjav2/interfaces/SFnDQSAPI.java
@@ -0,0 +1,9 @@
+package com.example.myapplicationjav2.interfaces;
+
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+
+interface SFnDQSAPI {
+    @Multipart
+    @POST("Api.")
+}
--- app/src/main/java/com/example/myapplicationjav2/ui/add_data/AddDataFragment.java
+++ app/src/main/java/com/example/myapplicationjav2/ui/add_data/AddDataFragment.java
@@ -1,15 +1,27 @@
 package com.example.myapplicationjav2.ui.add_data;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplicationjav2.R;
 import com.example.myapplicationjav2.databinding.FragmentAddDataBinding;
 
 public class AddDataFragment extends Fragment {
@@ -25,9 +37,71 @@ public class AddDataFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textAddData;
+
+        Button selDir = (Button) root.findViewById(R.id.button_sel_dir);
+        selDir.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                //OnCLick Stuff
+                openFolderChooser();
+            }
+        });
+
+        Button selFile = (Button) root.findViewById(R.id.button_sel_file);
+        selFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
         addDataViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    public void openFileChooser(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        selFileActivityLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> selFileActivityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    if(data == null){
+                        Toast.makeText(getActivity(),"Data is null",Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Uri uri = data.getData();
+                    Toast.makeText(getActivity(),uri.getPath(),Toast.LENGTH_LONG).show();
+                }
+            }
+    );
+
+    public void openFolderChooser(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        selDirActivityResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> selDirActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    if(data == null){
+                        Toast.makeText(getActivity(),"Data is null",Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Uri uri = data.getData();
+                    DocumentFile df = DocumentFile.fromTreeUri(getActivity().getApplicationContext(), uri);
+                    df.createFile("txt", "sample.txt");
+                    Toast.makeText(getActivity(),uri.getPath(),Toast.LENGTH_LONG).show();
+                }
+            }
+    );
+
+
+
 
     @Override
     public void onDestroyView() {
