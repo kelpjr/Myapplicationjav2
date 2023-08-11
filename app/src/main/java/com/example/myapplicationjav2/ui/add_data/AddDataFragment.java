cb4af4365a7873f003c3bbd14fa658e8c778f5cb
kelpjr <kelv0046@e.ntu.edu.sg>
2023-08-11 16:54:10 +0800

    Working on upload capabilities.

--- app/src/main/java/com/example/myapplicationjav2/ui/add_data/AddDataFragment.java
+++ app/src/main/java/com/example/myapplicationjav2/ui/add_data/AddDataFragment.java
@@ -1,9 +1,12 @@
 package com.example.myapplicationjav2.ui.add_data;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +17,37 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplicationjav2.R;
 import com.example.myapplicationjav2.databinding.FragmentAddDataBinding;
-import com.example.myapplicationjav2.services.RetrofitClient;
+import com.example.myapplicationjav2.services.FileUploadService;
+import com.example.myapplicationjav2.services.ServiceGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddDataFragment extends Fragment {
 
     private FragmentAddDataBinding binding;
     private AddDataViewModel addDataViewModel;
+
+    private Intent data;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -58,7 +80,124 @@ public class AddDataFragment extends Fragment {
             }
         });
 
+        Button upFile = (Button) root.findViewById(R.id.button_upload_files);
+        upFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile(data);
+            }
+        });
+
     }
+
+    private void uploadFile(Intent data) {
+        // create upload service client
+        FileUploadService service =
+                ServiceGenerator.createService(FileUploadService.class);
+
+        ArrayList<Uri> fileList = new ArrayList<Uri>();
+        if(data != null && data.getClipData() != null){
+            for (int i = 0; i < data.getClipData().getItemCount(); i++) {
+                fileList.add(data.getClipData().getItemAt(i).getUri());
+                File file = new File(createCopyAndReturnRealPath(getActivity(), fileList.get(i)));
+                RequestBody requestFile =
+                        RequestBody.create(
+                                MediaType.parse(getActivity().getContentResolver().getType(fileList.get(i))),
+                                file
+                        );
+
+                // MultipartBody.Part is used to send also the actual file name
+                MultipartBody.Part body =
+                        MultipartBody.Part.createFormData("picture", file.getName(), requestFile);
+
+                // add another part within the multipart request
+                String descriptionString = "hello, this is description speaking";
+                RequestBody description =
+                        RequestBody.create(
+                                okhttp3.MultipartBody.FORM, descriptionString);
+
+                // finally, execute the request
+                Call<ResponseBody> call = service.upload(description, body);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call,
+                                           Response<ResponseBody> response) {
+                        Log.v("Upload LOGGING", "success");
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("Upload error:", t.getMessage());
+                    }
+                });
+            }
+        }else{
+                File file = new File(createCopyAndReturnRealPath(getActivity(), data.getData()));
+                RequestBody requestFile =
+                        RequestBody.create(
+                                MediaType.parse(getActivity().getContentResolver().getType(data.getData())),
+                                file
+                        );
+
+                // MultipartBody.Part is used to send also the actual file name
+                MultipartBody.Part body =
+                        MultipartBody.Part.createFormData("picture", file.getName(), requestFile);
+
+                // add another part within the multipart request
+                String descriptionString = "hello, this is description speaking";
+                RequestBody description =
+                        RequestBody.create(
+                                okhttp3.MultipartBody.FORM, descriptionString);
+
+                // finally, execute the request
+                Call<ResponseBody> call = service.upload(description, body);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call,
+                                           Response<ResponseBody> response) {
+                        Log.v("Upload LOGGING", "success");
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("Upload error:", t.getMessage());
+                    }
+                });
+
+        }
+    }
+    @Nullable
+    public static String createCopyAndReturnRealPath(
+            @NonNull Context context, @NonNull Uri uri) {
+        final ContentResolver contentResolver = context.getContentResolver();
+        if (contentResolver == null)
+            return null;
+
+        // Create file path inside app's data dir
+        String filePath = context.getApplicationInfo().dataDir + File.separator
+                + System.currentTimeMillis();
+
+        File file = new File(filePath);
+        try {
+            InputStream inputStream = contentResolver.openInputStream(uri);
+            if (inputStream == null)
+                return null;
+
+            OutputStream outputStream = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0)
+                outputStream.write(buf, 0, len);
+
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException ignore) {
+            return null;
+        }
+
+        return file.getAbsolutePath();
+    }
+
     public void openFileChooser(){
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
@@ -68,8 +207,11 @@ public class AddDataFragment extends Fragment {
     ActivityResultLauncher<Intent> selFileActivityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
+                if (data != null) {
+                    data = null;
+                }
                 if(result.getResultCode() == Activity.RESULT_OK){
-                    Intent data = result.getData();
+                    data = result.getData();
                     if(data.getClipData() != null){
                         int count = data.getClipData().getItemCount();
                         addDataViewModel.setMText(Integer.toString(count) + " files selected!");
@@ -78,8 +220,10 @@ public class AddDataFragment extends Fragment {
 
                         addDataViewModel.setMText("1 file selected!");
 //                    Toast.makeText(getActivity(),uri.getPath(),Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(getActivity(),"Data is null!",Toast.LENGTH_LONG).show();
+
                     }
-                    Toast.makeText(getActivity(),"Data is null",Toast.LENGTH_LONG).show();
                 }
             }
 
